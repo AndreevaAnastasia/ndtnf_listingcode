f9121fa1b744d6e99ec9a7ffc7f78a3149decc82
Vladimir Yazykov <neizerth@gmail.com>
2022-01-22 13:40:52 +0300

    1 / 2

---
+++ 1.2/40-polymorphism/extended.ts
@@ -0,0 +1,24 @@
+class Book {
+    name: string;
+    author: string;
+    constructor(name: string, author: string) {
+        this.name = name;
+        this.author = author;
+    }
+    getInfo(): string {
+        return `${this.name} — ${this.author}`;
+    }
+}
+
+class ComicsBook extends Book {
+    type: string;
+    constructor(type: string, name: string, author: string) {
+        super(name, author);
+        this.type = type;
+    }
+    getInfo() {
+        const bookInfo: string = super.getInfo();
+        return `${bookInfo} — ${this.type}`;
+    }
+}
+
